efaec37abe342897172efb0879bd4e7b8fc1a95d
Himanshu Mishra <himanshu2014iit@gmail.com>
2017-12-09 04:00:56 +0530

    Fix imports and pep8 issues

--- kwoc/app.py
+++ kwoc/app.py
@@ -3,11 +3,10 @@ import collections
 import sys
 import os
 import json
-sys.path.append("kwoc")
-
 from flask import render_template, redirect
-import os
-import config
+from . import config
+
+sys.path.append("kwoc")
 
 app, sess = config.create_app()
 sess.init_app(app)
@@ -23,13 +22,19 @@ with open(stats_json, 'r') as f:
 non_zero_contributions = {}
 zero_contributions = {}
 for user, userdata in stats_dict.items():
-    if userdata['no_of_commits'] + userdata['pr_open'] + userdata['pr_closed'] == 0:
+    contribs = 0
+    contribs += userdata['no_of_commits']
+    contribs += userdata['pr_open']
+    contribs += userdata['pr_closed']
+    if contribs == 0:
         zero_contributions[user] = userdata
     else:
         non_zero_contributions[user] = userdata
 
-non_zero_contributions = collections.OrderedDict(sorted(non_zero_contributions.items(), key=lambda t: t[1]['name']))
-zero_contributions = collections.OrderedDict(sorted(zero_contributions.items(), key=lambda t: t[1]['name']))
+non_zero_contributions = collections.OrderedDict(
+    sorted(non_zero_contributions.items(), key=lambda t: t[1]['name']))
+zero_contributions = collections.OrderedDict(
+    sorted(zero_contributions.items(), key=lambda t: t[1]['name']))
 non_zero_contributions.update(zero_contributions)
 
 # Final data
@@ -76,26 +81,32 @@ def mentor_form():
     return "Registrations have now been closed. See you next year !"
     # return render_template('mentor_form.html')
 
+
 @app.route("/student_form")
 def student_form():
     return "Registrations have now been closed. See you next year !"
     # return render_template('student_form.html')
 
+
 @app.route("/projects")
 def projects():
     return render_template('projects.html')
+
 
 @app.route("/profile")
 def profile():
     return render_template('profile.html')
 
+
 mentors_json = root_dir + '/gh_scraper/list_of_mentors.json'
 with open(mentors_json, 'r') as f:
     list_of_mentors = json.load(f)
 
+
 @app.route("/mid-term")
 def mid_term():
-    return render_template('mid-term-student.html', list_of_mentors=list_of_mentors)
+    return render_template('mid-term-student.html',
+                           list_of_mentors=list_of_mentors)
 
 # # Lines below should not be needed for Python 3
 # from imp import reload
@@ -105,6 +116,7 @@ def mid_term():
 # except:
 #     pass
 # # above three lines are IMPORTANT
+
 
 if __name__ == '__main__' and "RUNNING_PROD" not in os.environ:
     app.run(host='0.0.0.0')
